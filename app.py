ec30fff4f9c390fa736f63a08c2faeb0316599b0
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2023-02-13 15:26:46 -0500

    Finishing touches

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ with app.app_context():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
+
+    class Todo(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(100))
+        complete = db.Column(db.Boolean)
+
     db.create_all()
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
-
-
 
 @app.get("/")
 def home():
